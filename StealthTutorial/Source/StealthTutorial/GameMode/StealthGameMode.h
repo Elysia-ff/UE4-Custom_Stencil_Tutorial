c83997e8cca3616b21c4b6f4b6c46595852626b5
Elysia-ff <jhk19960@gmail.com>
2021-05-23 21:14:44 +0900

    GameMode, PlayerController 생성

---
+++ StealthTutorial/Source/StealthTutorial/GameMode/StealthGameMode.h
@@ -0,0 +1,17 @@
+// Copyright 2021. Elysia-ff
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StealthGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEALTHTUTORIAL_API AStealthGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ StealthTutorial/Source/StealthTutorial/Player/StealthPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright 2021. Elysia-ff
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "StealthPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEALTHTUTORIAL_API AStealthPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
